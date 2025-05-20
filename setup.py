fce065e31a6a1afaa1e06e69234e02aaea50bdb0
Keyne Oei <keynekassapa13@gmail.com>
2025-05-20 10:42:12 +0200

    Add PyProject

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+from setuptools import setup, find_packages
+
+setup(
+    name="nodding",
+    version="0.1.0",
+    description="Detect nodding in a video.",
+    author="Keyne Oei",
+    author_email="keynekassapa13@gmail.com",
+    packages=find_packages(include=["nodding"]),
+    install_requires=[
+        "opencv-python",
+        "mediapipe",
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "loguru"
+    ],
+    entry_points={
+        "console_scripts": [
+            "nodding = nodding.cli:main"
+        ]
+    },
+    python_requires=">=3.8",
+)
+
